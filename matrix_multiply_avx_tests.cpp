70a44b051ee4f2248000b132e33ec9d39df2ca80
thwmakos <tomzach3@gmail.com>
2024-10-05 20:56:10 +0100

    more test cases

--- matrix_multiply_avx_tests.cpp
+++ matrix_multiply_avx_tests.cpp
@@ -38,11 +38,11 @@ TEST_CASE("test AVX512 matrix multiplication")
 	// a matrix with random values
 	std::random_device rd {}; 
 	std::default_random_engine eng { rd() };
-	std::normal_distribution<FloatType> normal(0.0f, 1.0f);
+	std::uniform_real_distribution<FloatType> uni(-10.0f, 10.0f);
 	
 	// lambda to randomise an sequence of matrices using 
 	// the random distribution constructed above
-	auto randomise = [&normal, &eng] (auto& mat)
+	auto randomise = [&uni, &eng] (auto& mat)
 	{
 		const auto [num_rows, num_cols] = mat.size();
 
@@ -50,7 +50,7 @@ TEST_CASE("test AVX512 matrix multiplication")
 		{
 			for(auto j = 0; j < num_cols; ++j)	
 			{
-				mat[i, j] = normal(eng);
+				mat[i, j] = uni(eng);
 			}
 		}
 	};
@@ -58,33 +58,45 @@ TEST_CASE("test AVX512 matrix multiplication")
 	// controls the size of matrices we multiply
 	constexpr int n = 8;
 	constexpr int m = 4 * 16;
-	constexpr int scale = 10;
-	constexpr int middle = 300;
-
-	matrix A(n * scale, middle);
-	matrix B(middle, m * scale);
-
-	randomise(A);
-	randomise(B);
 	
-	CHECK(multiply(A, B) == multiply_avx512(A, B));
+	// test various sizes
+	for(int scale : {3, 4, 5, 6, 7, 8, 9, 10})
+	{
+		for(int middle : {31, 42, 53, 57, 64, 65, 69})
+		{
+			matrix A(n * scale, middle);
+			matrix B(middle, m * (scale - 2));
+
+			randomise(A);
+			randomise(B);
+			
+			CHECK(multiply(A, B) == multiply_avx512(A, B));
+		}
+	}
+
 
 	// test speedup in optimised build
 	if constexpr(release_build)
 	{
-		matrix A_large(n * 50 * scale, 2 * middle); 
-		matrix B_large(2 * middle, m * 10 * scale); 
+		matrix A_large(n * 250, 2000); 
+		matrix B_large(2000, m * 32); 
+		randomise(A_large);
+		randomise(B_large);
 		auto t1 = std::chrono::high_resolution_clock::now();	
 		matrix C1 = multiply(A_large, B_large);
 		auto t2 = std::chrono::high_resolution_clock::now();
 		matrix C2 = multiply_avx512(A_large, B_large);
 		auto t3 = std::chrono::high_resolution_clock::now();
+		
+		auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+		auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count();
+
 
 		std::println("A dimensions: ({}, {}), B dimensions: ({}, {})", A_large.num_rows(), A_large.num_cols(), B_large.num_rows(), B_large.num_cols());
-		std::println("Naive multiply with transpose took {}", std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1));
-		std::println("AVX512 multiply took {}", std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2));
-		std::println("AVX512 was {} times faster", 
-				std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() / 
-				std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count());
+		std::println("Naive multiply with transpose took {}", duration1);
+		std::println("AVX512 multiply took {}", duration2);
+		std::println("AVX512 was {} times faster", duration1 / (duration2 != 0 ? duration2 : 1)); 
+		// add one above to avoid division by zero
+																							   
 	}
 }
