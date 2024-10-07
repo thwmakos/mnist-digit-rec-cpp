6d4d063891643556ba6f72e45686e3eb6b345183
thwmakos <tomzach3@gmail.com>
2024-10-07 10:54:40 +0100

    finish first version for avx512 matrix mutliplication for arbitrary matrix sizes

--- matrix_multiply_avx.cpp
+++ matrix_multiply_avx.cpp
@@ -9,19 +9,22 @@
 #include "matrix_multiply_avx.hpp"
 
 #include <cassert>
-#include <array>
 #include <immintrin.h>
 
+#include <array>
 #include <print>
+
+// TODO: update submatrix functions with std::array and std::span
 
 namespace thwmakos { 
 
 // we calculate the product C = A * B by computing
 // C in smaller submatrices of dimensions given
 // by the parameters below
-constexpr int num_submatrix_rows = 8;      // submatrix size needs to be adjusted to CPU
-constexpr int num_submatrix_cols = 4 * 16; // these parameters provide 20x performance boots to
-										   // naive implementation on intel tgl (i7 11800H CPU)
+constexpr int num_lanes = 16;
+constexpr int num_submatrix_rows = 8;             // submatrix size needs to be adjusted to CPU
+constexpr int num_submatrix_cols = 4 * num_lanes; // these parameters provide 20x performance boots to
+										          // naive implementation on intel tgl (i7 11800H CPU)
 // the number of columns is a multiple of 16 which 
 // is how many single precision floats an avx512 
 // register stores
@@ -47,7 +50,6 @@ void submatrix(const matrix &A, const matrix &B, matrix &C, int num_row, int num
 	// each row of b's will be loaded to a small number of __m512 variables
 	// equal to num_submatrix_cols / 16
 	
-	constexpr int num_lanes = 16;
 
 	__m512 C_submatrix[num_submatrix_rows][num_submatrix_cols / num_lanes];
 	__m512 a;
@@ -61,7 +63,7 @@ void submatrix(const matrix &A, const matrix &B, matrix &C, int num_row, int num
 	// we first load the submatrix of C into C_submatrix
 	for(int i = 0; i < num_submatrix_rows; ++i)
 	{
-		int C_load_index = C.index(num_row + i, num_col);
+		const int C_load_index = C.index(num_row + i, num_col);
 
 		for(int j = 0; j < num_submatrix_cols / num_lanes; ++j)
 		{
@@ -74,7 +76,7 @@ void submatrix(const matrix &A, const matrix &B, matrix &C, int num_row, int num
 	for(int i = 0; i < B.num_rows(); ++i)
 	{
 		// load num_submatrix_cols elements from the current row of B
-		int B_load_index = B.index(i, num_col);
+		const int B_load_index = B.index(i, num_col);
 		for(int j = 0; j < num_submatrix_cols / num_lanes; ++j)
 		{
 			b_row[j] = _mm512_loadu_ps(&B_data[B_load_index + num_lanes * j]);
@@ -83,7 +85,7 @@ void submatrix(const matrix &A, const matrix &B, matrix &C, int num_row, int num
 		// update each row of C_submatrix
 		for(int n = 0; n < num_submatrix_rows; ++n)
 		{
-			int A_load_index = A.index(num_row + n, i);
+			const int A_load_index = A.index(num_row + n, i);
 			a = _mm512_set1_ps(A_data[A_load_index]);
 
 			for(int j = 0; j < num_submatrix_cols / num_lanes; ++j)
@@ -96,14 +98,107 @@ void submatrix(const matrix &A, const matrix &B, matrix &C, int num_row, int num
 	// store results back to C
 	for(int i = 0; i < num_submatrix_rows; ++i)
 	{
-		int C_store_index = C.index(num_row + i, num_col);
+		const int C_store_index = C.index(num_row + i, num_col);
 
 		for(int j = 0; j < num_submatrix_cols / num_lanes; ++j)
 		{
 			_mm512_storeu_ps(&C_data[C_store_index + num_lanes * j], C_submatrix[i][j]);
 		}
 	}
+}
+
+// same as submatrix() except that we do not assume that we have num_submatrix_rows after num_row
+// or num_submatrix_cols after num_col
+void submatrix_masked(const matrix &A, const matrix &B, matrix &C, int num_row, int num_col)
+{
+	__m512 C_submatrix[num_submatrix_rows][num_submatrix_cols / num_lanes];
+	__m512 a;
+	__m512 b_row[num_submatrix_cols / num_lanes];
+	__mmask16 masks[num_submatrix_cols / num_lanes];
+
+	// get pointers to raw data first
+	const float *A_data = A.data().data();
+	const float *B_data = B.data().data();
+	float       *C_data = C.data().data();
 	
+	// we only process up to a submatrix size	
+	const int actual_rows = std::min(C.num_rows() - num_row, num_submatrix_rows);
+	const int actual_cols = std::min(C.num_cols() - num_col, num_submatrix_cols);
+	
+	// FIXME: this is redundant
+	if(actual_rows <= 0 || actual_cols <= 0)
+	{
+		return;
+	}
+
+	//assert(actual_rows <= num_submatrix_rows);
+	//assert(actual_cols <= num_submatrix_cols);
+	
+	// calculate masks for data loading/storing for C and B
+	for(int j = 0; j < num_submatrix_cols / num_lanes; ++j)
+	{
+		const int lanes_remaining = actual_cols - j * num_lanes;
+
+		if(lanes_remaining >= num_lanes)
+		{
+			// TODO: make this constant generic (determine it from num_lanes)
+			masks[j] = (1u << num_lanes) - 1u;
+		}
+		else if(lanes_remaining > 0)
+		{
+			masks[j] = (1u << lanes_remaining) - 1u;
+		}
+		else
+		{
+			masks[j] = 0u;
+		}
+	}
+	
+	// we first load the submatrix of C into C_submatrix
+	for(int i = 0; i < actual_rows; ++i)
+	{
+		const int C_load_index = C.index(num_row + i, num_col);
+
+		for(int j = 0; j < num_submatrix_cols / num_lanes; ++j)
+		{
+			C_submatrix[i][j] = _mm512_maskz_loadu_ps(masks[j], &C_data[C_load_index + num_lanes * j]);
+		}
+	}
+	
+	// loop over every row of C_submatrix calculating its value
+	// to do this loop over the rows of B as described above
+	for(int i = 0; i < B.num_rows(); ++i)
+	{
+		// load num_submatrix_cols elements from the current row of B
+		const int B_load_index = B.index(i, num_col);
+		for(int j = 0; j < num_submatrix_cols / num_lanes; ++j)
+		{
+			b_row[j] = _mm512_maskz_loadu_ps(masks[j], &B_data[B_load_index + num_lanes * j]);
+		}
+
+		// update each row of C_submatrix
+		for(int n = 0; n < actual_rows; ++n)
+		{
+			const int A_load_index = A.index(num_row + n, i);
+			a = _mm512_set1_ps(A_data[A_load_index]);
+
+			for(int j = 0; j < num_submatrix_cols / num_lanes; ++j)
+			{
+				C_submatrix[n][j] = _mm512_fmadd_ps(a, b_row[j], C_submatrix[n][j]);
+			}
+		}
+	}
+	
+	// store results back to C
+	for(int i = 0; i < actual_rows; ++i)
+	{
+		const int C_store_index = C.index(num_row + i, num_col);
+
+		for(int j = 0; j < num_submatrix_cols / num_lanes; ++j)
+		{
+			_mm512_mask_storeu_ps(&C_data[C_store_index + num_lanes * j], masks[j], C_submatrix[i][j]);
+		}
+	}
 }
 
 matrix multiply_avx512(const matrix &A, const matrix &B)
@@ -113,19 +208,34 @@ matrix multiply_avx512(const matrix &A, const matrix &B)
 
 	// no maskings loads/stores introduced yet, so we assume
 	// matrix dimensions satisfy the following for now
-	assert(A.num_rows() % num_submatrix_rows == 0);
-	assert(B.num_cols() % num_submatrix_cols == 0);
+	//assert(A.num_rows() % num_submatrix_rows == 0);
+	//assert(B.num_cols() % num_submatrix_cols == 0);
 
 	// matrix to be returned
 	matrix C(A.num_rows(), B.num_cols());
+
+	int i = 0;	
 	
-	for(int i = 0; i < C.num_rows(); i += num_submatrix_rows)
+	// make sure there are enough rows for a submatrix
+	for(; i + num_submatrix_rows <= C.num_rows(); i += num_submatrix_rows)
 	{
-		for(int j = 0; j < C.num_cols(); j += num_submatrix_cols)
+		int j = 0;
+		// if there are enough columns for a submatrix we call submatrix()
+		// otherwise we need to mask some columns at the trailing edge 
+		// so we call submatrix_masked()
+		for(; j + num_submatrix_cols <= C.num_cols(); j += num_submatrix_cols)
 		{
 			//std::println("submatrix starting at {}, {}", i, j);
 			submatrix(A, B, C, i, j);
 		}
+
+		submatrix_masked(A, B, C, i, j);
+	}
+
+	// handle any leftover columns
+	for(int j = 0; j < C.num_cols(); j += num_submatrix_cols)
+	{
+		submatrix_masked(A, B, C, i, j);
 	}
 
 	return C;
